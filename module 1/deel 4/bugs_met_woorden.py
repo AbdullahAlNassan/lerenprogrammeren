b3d86dd7792648990dabf7f8c25caf89d59e202d
a1o1o <99077332@mydavinci.nl>
2023-10-21 23:57:46 +0200

    push

--- module 1/deel 4/bugs_met_woorden.py
+++ module 1/deel 4/bugs_met_woorden.py
@@ -1,9 +1,9 @@
-a = str(input('Geef een woord'))
-b = str(input('Geef nog een woord'))
+a = input('Geef een woord: ')
+b = input('Geef nog een woord: ')
 
-if a < b:     
-    print(f'Woord {a} heeft meer letters dan woord {b}')
-elif a > b:   
+if (a) > (b):
     print(f'Woord {a} heeft minder letters dan woord {b}')
-else:                   
+elif (a) < (b):
+    print(f'Woord {a} heeft meer letters dan woord {b}')
+else:
     print(f'Woord {a} en woord {b} hebben evenveel letters')
