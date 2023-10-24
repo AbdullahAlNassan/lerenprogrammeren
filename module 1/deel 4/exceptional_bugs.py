eccb162c2cbd23f04ff27a407a9615ad4fc016df
a1o1o <99077332@mydavinci.nl>
2023-10-24 23:58:48 +0200

    push

--- module 1/deel 4/exceptional_bugs.py
+++ module 1/deel 4/exceptional_bugs.py
@@ -5,13 +5,13 @@ num1 = random.randint(1, 10)
 num2 = random.randint(5, 15)
 
 # Vraag om een antwoord
+number = input('Weet jij wat ' + str(num1) + ' + ' + str(num2) + ' is? ')
 
+# Geef reactie op het antwoord
 try:
-    number = int(input('Weet jij wat ' + str(num1) + ' + ' + str(num2) + ' is? '))
+    if int(number) == (num1 + num2):
+        print('Dat is juist')
+    elif int(number) != (num1 + num2):
+        print('Nee dat klopt niet')
 except ValueError:
     print('Dat is geen nummer!')
-# Geef reactie op het antwoord
-if number == (num1 + num2):
-     print('Dat is juist')
-elif number != (num1 + num2):
-    print('Nee dat klopt niet')
