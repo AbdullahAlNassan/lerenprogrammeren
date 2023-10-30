c1ee0613359287048631ba9a1a1cfcebadce7ded
a1o1o <99077332@mydavinci.nl>
2023-10-30 14:15:08 +0100

    added discounter

--- module 1/deel 5/real_function_2.py
+++ module 1/deel 5/real_function_2.py
@@ -3,37 +3,32 @@ from test_lib import test, report
 def afronden_naar_5_cent(getal):
     return round(getal * 20) / 20
 
-# Testgevallen en testen
+# testen
 original_getal = 2.24
 expected_getal = 2.25
 rounded_getal = afronden_naar_5_cent(original_getal)
-name = f"Original amount: {original_getal}, Rounded amount: {rounded_getal}"
-test(name, expected_getal, rounded_getal)
+test("test1", expected_getal, rounded_getal)
 
 original_getal = 13.01
 expected_getal = 13.0
 rounded_getal = afronden_naar_5_cent(original_getal)
-name = f"Original amount: {original_getal}, Rounded amount: {rounded_getal}" 
-test(name, expected_getal, rounded_getal)
+test("test2", expected_getal, rounded_getal)
 
 original_getal = 7.44
 expected_getal = 7.45
 rounded_getal = afronden_naar_5_cent(original_getal)
-name = f"Original amount: {original_getal}, Rounded amount: {rounded_getal}"
-test(name, expected_getal, rounded_getal)
+test("test3", expected_getal, rounded_getal)
 
 original_getal = 0.99
 expected_getal = 1.0
 rounded_getal = afronden_naar_5_cent(original_getal)
-name = f"Original amount: {original_getal}, Rounded amount: {rounded_getal}"
-test(name, expected_getal, rounded_getal)
+test("test4", expected_getal, rounded_getal)
 
 original_getal = 3.52
 expected_getal = 3.5
 rounded_getal = afronden_naar_5_cent(original_getal)
-name = f"Original amount: {original_getal}, Rounded amount: {rounded_getal}"
-test(name, expected_getal, rounded_getal)
+test("test5", expected_getal, rounded_getal)
 
-# Rapport genereren
+
 report()
 
---
+++ module 1/deel 5/real_function_3.py
@@ -0,0 +1,40 @@
+from test_lib import test, report
+
+month_discount_brands = 'Vespa,Kymco,Yamaha'
+MONTH_DISCOUNT_PERC = 10
+
+def calc_discount(price: float, brand: str, month_discount_brands: str) -> float:
+    if brand in month_discount_brands:
+        return round(price * MONTH_DISCOUNT_PERC / 100.0, 2)
+    else:
+        return 0.0
+
+
+# Testen
+price = 1000.0
+brand = 'Vespa'
+discount = calc_discount(price, brand, month_discount_brands)
+test("Test 1", 100.0, discount)
+
+price = 800.0
+brand = 'Kymco'
+discount = calc_discount(price, brand, month_discount_brands)
+test("Test 2", 80.0, discount)
+
+price = 1500.0
+brand = 'Yamaha'
+discount = calc_discount(price, brand, month_discount_brands)
+test("Test 3", 150.0, discount)
+
+price = 1200.0
+brand = 'Suzuki'  
+discount = calc_discount(price, brand, month_discount_brands)
+test("Test 4", 0.0, discount)
+
+price = 2000.0
+brand = 'Piaggio' 
+discount = calc_discount(price, brand, month_discount_brands)
+test("Test 5", 0.0, discount)
+
+
+report()
