c8bf61b98a31f349fb15cb9d023fae9030fbbff2
a1o1o <99077332@mydavinci.nl>
2023-11-30 14:07:33 +0100

    som 1000

---
+++ module 2/deel 3/Som_1000.py
@@ -0,0 +1,16 @@
+total_sum = 0
+current_number = 50
+iteration = 1
+
+while total_sum <= 1000:
+    total_sum += current_number
+    print(f"{iteration}.", end=" ")
+
+    for num in range(50, current_number + 1):
+        print(num, end=" ")
+        if num < current_number:
+            print("+", end=" ")
+
+    print(f"= {total_sum}")
+    current_number += 1
+    iteration += 1
