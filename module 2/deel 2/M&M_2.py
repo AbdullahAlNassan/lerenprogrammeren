bf6dd8d2052de2dada631171260423c11ee6a4ad
a1o1o <99077332@mydavinci.nl>
2023-11-30 11:34:13 +0100

    MnM2

---
+++ module 2/deel 2/M&M_2.py
@@ -0,0 +1,18 @@
+import random
+
+kleuren = ['rood', 'blauw', 'groen', 'geel', 'bruin']
+
+aantal_MnMs = int(input("Hoeveel M&M's wil je toevoegen aan de zak? "))
+
+zak_met_MnMs = {}
+
+for _ in range(aantal_MnMs):
+    kleur = random.choice(kleuren)
+    if kleur not in zak_met_MnMs:
+        zak_met_MnMs[kleur] = 1
+    else:
+        zak_met_MnMs[kleur] += 1
+
+print("Zak met M&M's:")
+for kleur, aantal in zak_met_MnMs.items():
+    print(f'{kleur} : {aantal}')
