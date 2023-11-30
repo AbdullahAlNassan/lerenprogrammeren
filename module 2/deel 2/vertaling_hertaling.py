d660a8a718e112cc4d7e8f11906080dbab6f3340
a1o1o <99077332@mydavinci.nl>
2023-11-30 11:54:44 +0100

    Vertaling : Hertaling

---
+++ module 2/deel 2/vertaling_hertaling.py
@@ -0,0 +1,25 @@
+def vertaal_tekst(tekst, woordenboek):
+    woorden = tekst.split()
+    vertaalde_tekst = []
+
+    for woord in woorden:
+        vertaal_woord = woordenboek.get(woord, woord)
+        vertaalde_tekst.append(vertaal_woord)
+
+    return ' '.join(vertaalde_tekst)
+
+vertaalwoorden = {
+    'hart': 'heart',
+    'auto': 'car',
+    'werk': 'work',
+    'hond': 'dog',
+    'vader': 'father',
+    'tas': 'bag',
+}
+
+input_tekst = input("Voer een stukje tekst in: ")
+
+vertaalde_tekst = vertaal_tekst(input_tekst, vertaalwoorden)
+
+print("Vertaalde tekst:")
+print(vertaalde_tekst)
