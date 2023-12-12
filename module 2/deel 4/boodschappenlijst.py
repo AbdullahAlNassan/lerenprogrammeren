491f5f04bb348793ce75d3235364bd347edba85d
a1o1o <99077332@mydavinci.nl>
2023-12-12 16:39:45 +0100

    Boodschappenlijstje

--- module 2/deel 4/boodschappenlijst.py
+++ module 2/deel 4/boodschappenlijst.py
@@ -1,46 +1,19 @@
-# Importeer de nodige modules
-import re
+boodschappenlijst = {}
 
-# Definieer een functie om een item toe te voegen aan de lijst
-def voeg_item_toe(lijst, item):
-    # Splits het item op in zijn twee delen
-    wat, hoeveel = item.strip().split("x")
-
-    # Vervang alle hoofdletters door kleine letters
-    wat = wat.lower()
-
-    # Controleer of het item al in de lijst staat
-    if wat in lijst:
-        # Zoek het item in de lijst
-        voorkomen = lijst.index(wat)
-
-        # Tel de hoeveelheid bij
-        lijst[voorkomen] = str(int(lijst[voorkomen]) + int(hoeveel))
-
-    # Voeg het item toe aan de lijst
-    else:
-        lijst.append(item)
-
-# Start het programma
-lijst = []
-
-# Vraag de gebruiker om items toe te voegen
 while True:
-    item = input("Wat wil je toevoegen aan je boodschappenlijst? ('Stop' om te stoppen): ")
+    item = input("Voer het item in: ")
+    hoeveelheid = int(input("Voer de hoeveelheid in: "))
 
-    # Controleer of de gebruiker wil stoppen
-    if item.lower() == "stop":
+    if item in boodschappenlijst:
+        boodschappenlijst[item] += hoeveelheid
+    else:
+        boodschappenlijst[item] = hoeveelheid
+
+    meer_toevoegen = input("Wil je meer items toevoegen? (ja/nee): ")
+
+    if meer_toevoegen != "ja":
         break
 
-
-    voeg_item_toe(lijst, item)
-
-# Toon de lijst
 print("[BOODSCHAPPENLIJST]")
-
-if not lijst:
-    print("Je boodschappenlijst is leeg.")
-else:
-    for item in lijst:
-        print(item)
-
+for item, hoeveelheid in boodschappenlijst.items:
+    print(f"{hoeveelheid}x {item}")
