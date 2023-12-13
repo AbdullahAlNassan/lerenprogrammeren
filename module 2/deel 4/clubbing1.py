a9082d4122d38ed8c40501cd2cde6e35a508a95f
a1o1o <99077332@mydavinci.nl>
2023-12-13 22:53:56 +0100

    bewerkingen

--- module 2/deel 4/clubbing1.py
+++ module 2/deel 4/clubbing1.py
@@ -7,10 +7,7 @@ DRANKJES = ('cola', 'bier', 'champagne')
 VIP_LIST = ('jeroen', 'jouke', 'rudi')
 
 KLEUR_BANDJE = 'Je krijgt van mij een {kleur} bandje'
-COMPLIMENTEN = 'Alsjeblieft complimenten van het huis'
-DRANKJE_PRIJS = 'Alsjeblieft, je {drankje}, dat is dan €{prijs}'
-LATER_PROBEREN = 'Probeer het over {0} jaar nog eens'
-GEEN_ALCOHOL = 'Sorry, je mag geen alcohol bestellen onder de 21'
+
 
 # Start programma
 print('Welkom in de kroeg!')
@@ -21,21 +18,21 @@ leeftijd = int(input('Hoe oud ben je? '))
 # Controleer leeftijd
 if leeftijd < 18:
     print('Sorry, je mag niet naar binnen.')
-    print(LATER_PROBEREN.format(18 - leeftijd))
+    print(f'Probeer het over {18 - leeftijd} jaar nog eens.')
     exit()
 
 # Vraag naar naam
 naam = input('Wat is je naam? ')
 
 # Controleer of het een vip is
-if naam in VIP_LIST and leeftijd >= 21:
+if naam not in VIP_LIST and leeftijd >= 21:
+    print('Je krijgt van mij een stempel')
+elif naam in VIP_LIST and leeftijd >= 21:
     kleur = 'blauw'
-    print(KLEUR_BANDJE.format(kleur=kleur))
 elif naam in VIP_LIST and leeftijd < 21:
     kleur = 'rood'
-    print(KLEUR_BANDJE.format(kleur=kleur))
-elif naam not in VIP_LIST and leeftijd >= 21:
-    print('Je krijgt van mij een stempel')
+
+print(f'Je krijgt van mij een {kleur} bandje')
 
 
 
@@ -46,25 +43,24 @@ drankje = input('Wat wil je drinken? ')
 if drankje not in DRANKJES:
     print('Sorry, ik weet niet wat je bedoelt. Hier is een glaasje water.')
 else:
-    # Controleer leeftijd en vip-status
-    if drankje == 'bier' and leeftijd < 21:
-        print(GEEN_ALCOHOL.format(21 - leeftijd))
-        print(LATER_PROBEREN.format(21 - leeftijd))
-    elif drankje == 'bier' and leeftijd >= 21 and naam not in VIP_LIST:
-        print(DRANKJE_PRIJS.format(drankje=drankje, prijs=PRIJS_BIER))
-    elif drankje == 'bier' and naam in VIP_LIST and kleur == 'blauw':
-        print(COMPLIMENTEN)
+    if (drankje == 'bier' and leeftijd < 21) or (drankje == 'champagne' and naam in VIP_LIST and kleur != 'blauw'):
+        print('Sorry, je mag geen alcohol bestellen onder de 21')
+        print(f'Probeer het over {21 - leeftijd} jaar nog eens.')
+
+    elif (drankje == 'bier' and leeftijd >= 21 and naam not in VIP_LIST) or (drankje == 'champagne' and naam in VIP_LIST and kleur == 'blauw') or (drankje == 'cola' and naam not in VIP_LIST):
+        if drankje == 'bier':
+            prijs = PRIJS_BIER
+        elif drankje == 'cola':
+            prijs = PRIJS_COLA
+        elif drankje == 'champagne':
+            prijs = PRIJS_CHAMPAGNE
+        print(f'Alsjeblieft, je {drankje}, dat is dan €{prijs}')
+
+    elif (drankje == 'bier' and naam in VIP_LIST and kleur == 'blauw') or (drankje == 'cola' and naam in VIP_LIST):
+        print('Alsjeblieft complimenten van het huis')
+
     elif drankje == 'champagne' and naam not in VIP_LIST:
         print('Sorry, alleen vips mogen champagne bestellen.')
-    elif drankje == 'champagne' and naam in VIP_LIST and kleur != 'blauw':
-        print(GEEN_ALCOHOL.format(21 - leeftijd))
-        print(LATER_PROBEREN.format(21 - leeftijd))
-    elif drankje == 'champagne' and naam in VIP_LIST and kleur == 'blauw':
-        print(DRANKJE_PRIJS.format(drankje=drankje, prijs=PRIJS_CHAMPAGNE))
-    elif drankje == 'cola' and naam in VIP_LIST:
-        print(COMPLIMENTEN)
-    else:
-        print(DRANKJE_PRIJS.format(drankje=drankje, prijs=PRIJS_COLA))
+        
 
-# Einde programma
 print('Tot ziens!')
