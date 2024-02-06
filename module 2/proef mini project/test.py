245cf3c495ddd6a6e6fe8440f2c9a9f09969c16e
a1o1o <99077332@mydavinci.nl>
2024-02-06 13:21:10 +0100

    eerste code van proef code

--- module 2/proef mini project/test.py
+++ module 2/proef mini project/test.py
@@ -1,34 +1,58 @@
+# import random
+
+# unique_values = set()
+
+# while True:
+#     deelnemer = input("Voer een naam in: ")
+
+#     if deelnemer in unique_values:
+#         print("Waarde is niet uniek. Probeer het opnieuw.")
+#     else:
+#         unique_values.add(deelnemer)
+#         print("Waarde succesvol toegevoegd.")
+
+#     if len(unique_values) >= 3:
+#         meer_namen = input("Wil je meer namen toevoegen? (ja of nee) ")
+#         if meer_namen == "nee":
+#             break
+
+# deelnemers = list(unique_values)
+# lootjes = list(deelnemers)
+# random.shuffle(lootjes)
+
+# niemand_heeft_zijn_eigen_lootje = True
+# for i in range(len(deelnemers)):
+#     if deelnemers[i] == lootjes[i]:
+#         niemand_heeft_zijn_eigen_lootje = False
+#         break
+
+# if not niemand_heeft_zijn_eigen_lootje:
+#     print("Herstart het programma en probeer opnieuw.")
+# else:
+#     for i in range(len(deelnemers)):
+#         print(f"{deelnemers[i]} koopt voor: {lootjes[(i + 1) % len(deelnemers)]}")
+
 import random
 
-unique_values = set()
+deelnemers = []
 
 while True:
     deelnemer = input("Voer een naam in: ")
 
-    if deelnemer in unique_values:
+    if deelnemer in deelnemers:
         print("Waarde is niet uniek. Probeer het opnieuw.")
     else:
-        unique_values.add(deelnemer)
+        deelnemers.append(deelnemer)
         print("Waarde succesvol toegevoegd.")
 
-    if len(unique_values) >= 3:
+    if len(deelnemers) >= 3:
         meer_namen = input("Wil je meer namen toevoegen? (ja of nee) ")
         if meer_namen == "nee":
             break
 
-deelnemers = list(unique_values)
-lootjes = list(deelnemers)
-random.shuffle(lootjes)
+lootjes = deelnemers[1:] + deelnemers[:1]  # Dit is de "cirkelvormige rotatie"
 
-niemand_heeft_zijn_eigen_lootje = True
 for i in range(len(deelnemers)):
-    if deelnemers[i] == lootjes[i]:
-        niemand_heeft_zijn_eigen_lootje = False
-        break
+    print(f"{deelnemers[i]} koopt voor: {lootjes[i]}")
 
-if not niemand_heeft_zijn_eigen_lootje:
-    print("Herstart het programma en probeer opnieuw.")
-else:
-    for i in range(len(deelnemers)):
-        print(f"{deelnemers[i]} koopt voor: {lootjes[(i + 1) % len(deelnemers)]}")
 
